a28b5fa2753b42211bdda802ac18d63f6e2538ed
austin <austin.grubb.1995@gmail.com>
2018-10-15 14:40:08 -0700

    fixed login

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import { AppContainer } from 'react-hot-loader';
-// import { Provider } from 'react-redux';
-// import store from './ducks/store';
+import { Provider } from 'react-redux';
+import store from './ducks/store';
 
 const render = () => {
     ReactDOM.render(
         <AppContainer>
+          <Provider store={store}>
                 <BrowserRouter>
                         <App />
                 </BrowserRouter>
+          </Provider>
         </AppContainer>
     , document.getElementById('root'));
 }
